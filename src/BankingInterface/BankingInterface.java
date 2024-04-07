c385a91ac3bf4159180769f91d302017323b3d2a
pks21g <paulsmith414@yahoo.com>
2024-04-06 20:44:42 -0500

    Reimplemented Banking interface

--- src/BankingInterface/BankingInterface.java
+++ src/BankingInterface/BankingInterface.java
@@ -1,21 +1,15 @@
 package BankingInterface;
 
-import java.util.List;
-
 public interface BankingInterface {
-    enum accountType {
-        CHECKING,
-        SAVING,
-        STUDENT_CHECKING
-    }
-    void createAccount(int accountHolderName, accountType accountType, double initialDeposit);
-    void deposit(int accountNumber, double depositAmount);
-    void withdraw(int accountNumber, double withdrawalAmount);
-    void checkBalance(int accountNumber);
-    void getTransactionHistory();
-    void closeAccount(int accountNumber);
-    void Transfer(int fromAccountNo, int toAccountNo, double amount);
-    
+
+    void createAccount(String accountHolderName, String accountType, double initialDeposit);
+    boolean deposit(int accountNumber, double amount);
+    boolean withdraw(int accountNumber, double amount);
+    boolean getAccountInfo(int accountNumber);
+    double getBalance(int accountNumber);
+    void getTransactionsHistory(int accountNumber);
+
+
 
 
 }
